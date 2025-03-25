cac538cbfd5908aa9cdf1c830fdd13182c178c0d
David Tonon <david15tonon@gmail.com>
2025-03-25 21:46:57 +0100

    utils added

--- problem_24.js
+++ problem_24.js
@@ -1,26 +1,4 @@
-function lexicographicPermutations(n) {
-    let digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    let result = '';
-    n = n - 1;
+import {Abstractsolution, Runsolution} from "../../utils/solution";
+@Runsolution
+export class Solution24 extends Abstractsolution {
     
-    for (let i = digits.length - 1; i >= 0; i--) {
-        let factorial = 1;
-        for (let j = 1; j <= i; j++) {
-            factorial *= j;
-        }
-        
-        let index = Math.floor(n / factorial);
-        n = n % factorial;
-        
-        result += digits[index];
-        digits.splice(index, 1);
-    }
-    
-    return parseInt(result, 10); // Convert the result string to a number
-}
-
-// Tests
-console.log(lexicographicPermutations(699999)); // 1938246570
-console.log(lexicographicPermutations(899999)); // 2536987410
-console.log(lexicographicPermutations(900000)); // 2537014689
-console.log(lexicographicPermutations(999999)); // 2783915460
